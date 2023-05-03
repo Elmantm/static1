4adc1542f490b204d7354d828023771581499989
ElmanManmedov <elmantm@code.edu.az>
2023-05-03 15:19:40 +0400

    created unstatic Id task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp14;
+
+Console.WriteLine("Hello, World!");
+Student n10 = new();
+Student n20 = new();
+Student n30 = new();
+Student n40 = new();
+Student n50 = new();
+Student n60 = new();
+Student n70 = new();
+Student n80 = new();
+Student n90 = new();
+Student n100 = new();
+
+Console.WriteLine(n10.Id);
+Console.WriteLine(n20.Id);
+Console.WriteLine(n30.Id);
+Console.WriteLine(n40.Id);
+Console.WriteLine(n50.Id);
+Console.WriteLine(n60.Id);
+Console.WriteLine(n70.Id);
+Console.WriteLine(n80.Id);
+Console.WriteLine(n90.Id);
+Console.WriteLine(n100.Id);
+
+
+//string sentence = "I go to the school.Then I go to the home.Finally I eat food and go to the bed.go";
+//int result = sentence.MatchCount("go");
+//Console.WriteLine(result);
---
+++ student.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp14
+{
+    internal class Student
+    {
+        public static int Count { get; set; }
+        public int Id { get; set; }
+        public Student()
+        {
+            Count++;
+            Id = Count;
+        }
+    }
+
+}
